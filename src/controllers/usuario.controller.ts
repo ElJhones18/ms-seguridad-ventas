b60a38563a8c2f0b6899f36eb0fdb16f02b36df6
Jhon <jhon.rodriguez22329@ucaldas.edu.co>
2023-03-02 17:05:21 -0500

    agregacion de funcionalidad login con 2fa

--- src/controllers/usuario.controller.ts
+++ src/controllers/usuario.controller.ts
@@ -17,18 +17,21 @@ import {
   del,
   requestBody,
   response,
+  HttpErrors,
 } from '@loopback/rest';
-import {Usuario} from '../models';
-import {UsuarioRepository} from '../repositories';
+import {Credenciales, FactorDeAutenticacionPorCodigo, Login, Usuario} from '../models';
+import {LoginRepository, UsuarioRepository} from '../repositories';
 import {SeguridadUsuarioService} from '../services';
 
 export class UsuarioController {
   constructor(
     @repository(UsuarioRepository)
-    public usuarioRepository : UsuarioRepository,
+    public usuarioRepository: UsuarioRepository,
     @service(SeguridadUsuarioService)
-    public servicioSeguridad: SeguridadUsuarioService
-  ) {}
+    public servicioSeguridad: SeguridadUsuarioService,
+    @repository(LoginRepository)
+    public repositorioLogin: LoginRepository
+  ) { }
 
   @post('/usuario')
   @response(200, {
@@ -49,7 +52,7 @@ export class UsuarioController {
     usuario: Omit<Usuario, '_id'>,
   ): Promise<Usuario> {
     // crear la clave
-    let clave = this.servicioSeguridad.crearClave();
+    let clave = this.servicioSeguridad.crearTextoAleatorio(10);
     console.log(clave);
     // cifrar la clave
     let claveCifrada = this.servicioSeguridad.cifrarTexto(clave);
@@ -158,5 +161,86 @@ export class UsuarioController {
   })
   async deleteById(@param.path.string('id') id: string): Promise<void> {
     await this.usuarioRepository.deleteById(id);
+  }
+
+  /**
+   * Métodos personalizados para la API
+   */
+
+  @post('/identificar-usuario')
+  @response(200, {
+    description: "Identificar un usuario por correo y clave",
+    content: {'application/json': {schema: getModelSchemaRef(Credenciales)}}
+  })
+  async identificarUsuario(
+    @requestBody(
+      {
+        content: {
+          'application/json': {
+            schema: getModelSchemaRef(Credenciales)
+          }
+        }
+      }
+    )
+    credenciales: Credenciales
+  ): Promise<object> {
+    let usuario = await this.servicioSeguridad.identificarUsuario(credenciales);
+    if (usuario) {
+      let codigo2fa = this.servicioSeguridad.crearTextoAleatorio(5);
+      console.log(codigo2fa);
+      let login: Login = new Login();
+      login.usuarioId = usuario._id!;
+      login.codigo2fa = codigo2fa;
+      login.estadoCodigo2fa = false;
+      login.token = "";
+      login.estadoToken = false;
+      this.repositorioLogin.create(login);
+      usuario.clave = ""
+      // notificar al usuario
+      return usuario
+    }
+    return new HttpErrors[401]("Credenciales incorrectas.");
+  }
+
+  @post('/verificar-2fa')
+  @response(200, {
+    description: "Validar un codigo 2fa"
+  })
+  async VerificarCodigo2fa(
+    @requestBody(
+      {
+        content: {
+          'application/json': {
+            schema: getModelSchemaRef(FactorDeAutenticacionPorCodigo)
+          }
+        }
+      }
+    )
+    credenciales: FactorDeAutenticacionPorCodigo
+  ): Promise<object> {
+    let usuario = await this.servicioSeguridad.validarCodigo2fa(credenciales);
+    if (usuario) {
+      let token = this.servicioSeguridad.crearToken(usuario)
+      if (usuario) {
+        usuario.clave = "";
+        try {
+          this.usuarioRepository.logins(usuario._id).patch({
+            estadoCodigo2fa:true,
+            token: token
+          },
+          {
+            estadoCodigo2fa:false
+          })
+        } catch (error) {
+          console.log("No se ha almacenado el cambio del estado de token en la base de datos");
+        }
+
+        return {
+          user: usuario,
+          token: token
+      }
+      }
+    }
+    return new HttpErrors[401]("Código de 2fa invalido para el usuario definido.");
   }
 }
--- src/services/seguridad-usuario.service.ts
+++ src/services/seguridad-usuario.service.ts
@@ -1,24 +1,90 @@
 import {injectable, /* inject, */ BindingScope} from '@loopback/core';
+import {repository} from '@loopback/repository';
+import {ConfiguracionSeguridad} from '../config/seguridad.config';
+import {Credenciales, FactorDeAutenticacionPorCodigo, Login, Usuario} from '../models';
+import {LoginRepository, UsuarioRepository} from '../repositories';
 const generator = require('generate-password');
 const MD5 = require('crypto-js/md5');
+const jwt = require('jsonwebtoken');
 
 @injectable({scope: BindingScope.TRANSIENT})
 export class SeguridadUsuarioService {
-  constructor(/* Add @inject to inject parameters */) { }
+  constructor(
+    @repository(UsuarioRepository)
+    public repositorioUsuario: UsuarioRepository,
+    @repository(LoginRepository)
+    public repositorioLogin: LoginRepository
+  ) { }
 
-  /*
-   * Add service methods here
+  /**
+   * Crea una cadena aleatoria
+   * @returns cadena aleatorioa de n caracteres
    */
-  crearClave(): string{
-    let clave = generator.generate({
-      lenght: 10,
+  crearTextoAleatorio(n: number): string {
+    let textoAleatorioi = generator.generate({
+      lenght: n,
       numbers: true
     });
-    return clave
+    return textoAleatorioi
   }
 
-  cifrarTexto(cadena:string): string{
+  /**
+   * Cifrar una cadena con método MD5
+   * @param cadena texto a cifrar
+   * @returns cadena cifrada con md5
+   */
+  cifrarTexto(cadena: string): string {
     let cadenaCifrada = MD5(cadena).toString();
     return cadenaCifrada
+  }
+
+  /**
+   * Busca un suario por sus credenciales de acceso
+   * @param credenciales credenciales de usuario
+   * @returns usuario encontrado o null
+   */
+  async identificarUsuario(credenciales: Credenciales): Promise<Usuario | null> {
+    let usuario = await this.repositorioUsuario.findOne({
+      where: {
+        correo: credenciales.correo,
+        clave: credenciales.clave
+      }
+    });
+    return usuario as Usuario;
+  }
+
+  /**
+   * valida un codigo2fa para el usuario
+   * @param credenciales2fa credenciales del usuario con el codigo2fa
+   * @returns registro de login o null
+   */
+  async validarCodigo2fa(credenciales2fa: FactorDeAutenticacionPorCodigo): Promise<Usuario | null> {
+    let login = await this.repositorioLogin.findOne({
+      where: {
+        usuarioId: credenciales2fa.usuarioId,
+        codigo2fa: credenciales2fa.codigo2fa,
+        estadoCodigo2fa: false
+      }
+    });
+    if (login) {
+      let usuario = this.repositorioUsuario.findById(credenciales2fa.usuarioId);
+      return usuario;
+    }
+    return null;
+  }
+
+  /**
+   * Generacion del jwt
+   * @param usuario informacion del usuario
+   * @returns token
+   */
+  crearToken(usuario: Usuario): string {
+    let datos = {
+      name: `${usuario.primerNombre} ${usuario.segundoNombre} ${usuario.primerApellido} ${usuario.segundoApellido}`,
+      role: usuario.rolId,
+      email: usuario.correo
+    };
+    let token = jwt.sign(datos, ConfiguracionSeguridad.claveJWT);
+    return token
   }
 }
